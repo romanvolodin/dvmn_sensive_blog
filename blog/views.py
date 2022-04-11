269d5ceeb82e50a85f95daad04e723b72b6ab0bb
Roman Volodin <roman.volodin@gmail.com>
2022-04-11 20:17:58 +0300

    Remove unused code

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,6 @@
 from django.db.models import Count, Prefetch
 from django.shortcuts import render
-from blog.models import Comment, Post, Tag
-
-
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
+from blog.models import Post, Tag
 
 
 def serialize_post_optimized(post):
