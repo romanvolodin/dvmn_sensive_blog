dac0eb910e6cf2f612b0fb4035da248bde8b985a
Roman Volodin <roman.volodin@gmail.com>
2022-04-12 00:25:48 +0300

    Use decorator for admin models registration

--- blog/admin.py
+++ blog/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     raw_id_fields = ("post", "author")
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ("author", "likes", "tags")
 
 
-admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
-admin.site.register(Comment, CommentAdmin)
